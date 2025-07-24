d0d561d448d8b1f686afa4de566d09825a41718b
EF-Code <111162892+EF-Code@users.noreply.github.com>
2025-07-24 23:44:12 +0100

    add dashboard

--- inventory_app.py
+++ inventory_app.py
@@ -9,3 +9,17 @@ app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'inventory_db'
 
 mysql = MySQL(app)
+
+@app.route('/')
+def dashboard():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT COUNT(*) FROM items")
+    total_items = cur.fetchone()[0]
+
+    cur.execute("SELECT * FROM items WHERE quantity < 5")
+    low_stock = cur.fetchall()
+
+    cur.execute("SELECT * FROM items ORDER BY id DESC LIMIT 5")
+    recent = cur.fetchall()
+    cur.close()
+    return render_template('dashboard.html', total=total_items, low_stock=low_stock, recent=recent)
